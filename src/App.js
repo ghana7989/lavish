300925a99ffe48ee9d7c2432190c3ae1c2e77f83
ghana7989 <ghana.chindukuri@gmail.com>
2020-09-25 12:11:21 +0530

    lavish

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/lavish' exact component={Home} />
         <Route path='/services' component={Services} />
         <Route path='/products' component={Products} />
         <Route path='/sign-up' component={SignUp} />
